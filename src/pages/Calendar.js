764937ac4b7651c9e918260daa0d455d7c51eabc
Hammad Khan <khanhammad896@gmail.com>
2021-09-16 20:01:44 +0500

    Made some changes

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,7 +1,36 @@
 import React from "react";
-
-const Reminders = () => {
-  return <section className="tab">Calendar</section>;
+import styled from "styled-components";
+import Appbar from "../components/Appbar";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+const Calendar = () => {
+  return (
+    <section className="tab">
+      <CalendarWrapper>
+        <Appbar />
+        <div className="month-select-container">
+          <Select defaultValue={`June 2021`}>
+            <MenuItem>June 2021</MenuItem>
+            <MenuItem>July 2021</MenuItem>
+            <MenuItem>August 2021</MenuItem>
+          </Select>
+        </div>
+      </CalendarWrapper>
+    </section>
+  );
 };
 
-export default Reminders;
+export default Calendar;
+
+const CalendarWrapper = styled.div`
+  width: 100%;
+  margin-inline: auto;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+
+  .month-select-container {
+    margin-block-start: 20px;
+  }
+`;
